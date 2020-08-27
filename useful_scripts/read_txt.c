05350b656e58fc05178c452172e1f37f4cc3121f
Zhuo Zhang <imzhuo@foxmail.com>
2020-08-27 14:18:27 +0800

    update scripts

---
+++ useful_scripts/read_txt.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+void read_txt(const char* path)
+{
+    FILE* fin = fopen(path, "r");
+    if(fin==NULL) {
+        fprintf(stderr, "failed to open %s\n", path);
+        exit(-1);
+    }
+    char line[1024];
+    void* ret;
+    while (true) {
+        memset(line, 0, sizeof(line));
+        ret = fgets(line, sizeof(line), fin);
+        if (ret == NULL) break;
+        if(line[strlen(line)-1]=='\n' || line[strlen(line)-1]=='\r') {
+            line[strlen(line) - 1] = '\0';
+        }
+        if(line[strlen(line)-1]=='\r' || line[strlen(line)-1]=='\n') {
+            line[strlen(line) - 1] = '\0';
+        }
+        printf("%s\n", line);
+    }
+    fclose(fin);
+}
+
+
+int main() {
+    read_txt("input.txt");
+    return 0;
+}
