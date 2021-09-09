ff4a5b80853a22f048725f323f9440565e0cfc6e
Zhuo Zhang <imzhuo@foxmail.com>
2021-09-09 22:59:01 +0800

    feat: add fuck_github.sh

---
+++ useful_scripts/fuck_github.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#当执行git push，发现如下提示
+#remote: Support for password authentication was removed on August 13, 2021. Please use a personal access token instead.
+#remote: Please see https://github.blog/2020-12-15-token-authentication-requirements-for-git-operations/ for more information.
+#fatal: 无法访问 'https://github.com/zchrissirhcz/cmake_examples/'：The requested URL returned error: 403
+#
+# 原因：github不让本地的老repo用 https 协议执行push动作了。解决办法是改https为git：
+
+#remote=`git remote -v | head -1 | awk '{print $2}'`
+#echo $remote
+
+new_url=`git remote -v | head -1 | awk '{print $2}' | sed -e 's/https:\/\//git@/g' -e 's/github.com\//github.com:/g'`
+
+git remote set-url origin $new_url
