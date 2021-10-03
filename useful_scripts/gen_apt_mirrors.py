e74dc6a0a2e278af9e2cc14e3cd3d261abef5868
Zhuo Zhang <imzhuo@foxmail.com>
2021-10-04 02:03:16 +0800

    feat: update mytest.py, more like gtest

--- useful_scripts/gen_apt_mirrors.py
+++ useful_scripts/gen_apt_mirrors.py
@@ -49,5 +49,5 @@ def get_mirror_site(dist_num_str='16.04', site_name='aliyun'):
         print(mirror_url)
 
 if __name__ == '__main__':
-    get_mirror_site(dist_num_str='20.04', site_name='tuna')
+    get_mirror_site(dist_num_str='20.04', site_name='huawei')
 
