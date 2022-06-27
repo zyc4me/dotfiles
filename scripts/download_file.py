f266759881cc3e110b5eaafc393b0ff428aa327d
Zhuo Zhang <imzhuo@foxmail.com>
2022-06-27 17:36:53 +0800

    feat: Support download file with authentication

--- scripts/download_file.py
+++ scripts/download_file.py
@@ -26,3 +26,37 @@ def download_file(url, save_file=None):
                     desc=save_file) as fout:
         for chunk in response.iter_content(chunk_size=4096):
             fout.write(chunk)
+
+
+def download_file_with_authentication(url, username, password, save_file = None):
+    print("Downloading {:s} ...".format(url))
+
+    if save_file is None:
+        save_file = url.split('/')[-1]
+    if os.path.exists(save_file) is True:
+        print("  local file {:s} already exist, skip download".format(save_file))
+        return
+
+    response = requests.get(url, auth=(username, password))
+    status = response.status_code
+    if (status != 200):
+        print("  download file failed, return code ", status)
+        return False
+
+    # with open(save_file, 'wb') as f:
+    #     f.write(response.content)
+    with open(save_file, "wb") as fout:
+        for chunk in response.iter_content(chunk_size=4096):
+            fout.write(chunk)
+
+    print("  download ok, saved as {:s}".format(url, save_file))
+
+    return True
+
+
+def test_download_with_authentication():
+    url = 'https://artifactory.arcsoft.com.cn/artifactory/generic/arcpkg/toy_core/1.1.35/toy_core-1.1.35-android-armv8-a-static.zip'
+    download_file_with_authentication(url, os.environ.get("ARTIFACTORY_USER"), os.environ.get("ARTIFACTORY_PSWD"))
+
+if __name__ == '__main__':
+    test_download_with_authentication()
