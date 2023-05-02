35c9033032898d97a4ce95a9a0593500ba597065
Zhuo Zhang <imzhuo@foxmail.com>
2023-05-02 18:25:46 +0800

    fix color theme install

--- Windows/install.sh
+++ Windows/install.sh
@@ -4,11 +4,24 @@
 
 cp _vimrc ~/_vimrc
 
-mkdir -p ~/.vim/color
-cp onedark.vim ~/.vim/color/
-cp molokai.vim ~/.vim/color/
+mkdir -p ~/.vim/colors
+cp colors/* ~/.vim/colors/
 
 mkdir -p ~/.vim/temp_dirs/undodir
 
-echo "OK"
+echo "vim OK"
+
+#----------
+NVIM_CFG_DIR=~/AppData/Local/nvim
+mkdir -p $NVIM_CFG_DIR
+cp nvim/init.lua $NVIM_CFG_DIR/init.lua
+
+mkdir -p $NVIM_CFG_DIR/colors
+cp colors/* $NVIM_CFG_DIR/colors/
+
+mkdir -p $NVIM_CFG_DIR/temp_dirs/undodir
+
+echo "nvim OK"
+
+
 
