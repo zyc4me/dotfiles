11a6ae69ff2479f4aab93aed19b1aa421e808e8f
Zhuo Zhang <imzhuo@foxmail.com>
2023-05-17 00:13:48 +0800

    gen table for ndk download links

--- scripts/get_android_ndk_urls.py
+++ scripts/get_android_ndk_urls.py
@@ -20,7 +20,54 @@ def get_ndk_url(os_name, ndk_version):
 
     # url = 'https://dl.google.com/android/repository/android-ndk-r23-linux.zip'
     url = 'https://dl.google.com/android/repository/android-ndk-{}-{}.zip'.format(ndk_version, os_name)
-    print(url)
+    return url
+
+
+
+class CodeWriter(object):
+    def __init__(self, indent_len):
+        self.lines = []
+        self.indent_num = 0
+        self.indent_len = indent_len
+
+    def write(self, content):
+        padding = (self.indent_len * self.indent_num) * ' '
+        line = padding + content
+        self.lines.append(line)
+
+    def save(self, filename):
+        with open(filename, "w") as fout:
+            for line in self.lines:
+                fout.write(line + "\n")
+
+    def dump(self):
+        for line in self.lines:
+            print(line)
+
+    def tab(self):
+        self.indent_num += 1
+
+    def backspace(self):
+        if (self.indent_num > 0):
+            self.indent_num -= 1
+
+
 
 if __name__ == '__main__':
-    url = get_ndk_url('linux', 'r23')
+    #url = get_ndk_url('linux', 'r23')
+    os_names = ['linux', 'windows', 'darwin']
+    w = CodeWriter(4)
+    head = '| ndk version |'
+    sepline = '| ------ |'
+    for os_name in os_names:
+        head += ' {:s} |'.format(os_name)
+        sepline += ' ---------- |'
+    w.write(head)
+    w.write(sepline)
+    for ndk_version in ['r17', 'r18', 'r19', 'r20', 'r21', 'r22', 'r23', 'r24', 'r25']:
+        line = "| " + ndk_version + " |"
+        for os_name in os_names:
+            url = get_ndk_url(os_name, ndk_version)
+            line += ' <' + str(url) + '> |'
+        w.write(line)
+    w.dump()
